ad098cc48f81bd8ea2bbb03136d444eea02783ed
Przemek Dolata <przemekdolata96@gmail.com>
2018-11-13 16:26:34 +0100

    Added more products details, style ProductDetails component

--- components/ProductDetail.js
+++ components/ProductDetail.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Modal, View, Image, Text, Button, StyleSheet } from 'react-native';
+import { PieChart } from 'react-native-svg-charts';
 
 const productDetail = props => {
   let modalContent = null;
+  let protein = null;
+  let carbons = null;
+  let fats = null;
+  let ingredients = null;
 
   if (props.selectedProduct) {
     modalContent = (
@@ -11,14 +16,61 @@ const productDetail = props => {
         <Text style={styles.productName}>{props.selectedProduct.name}</Text>
       </View>
     );
-  };
 
+    ingredients = (
+       <View style={styles.ingredientsContainer}>
+          {
+            props.selectedProduct.ingredients.map((element) => (
+              <Text style={styles.ingredient} key={element}>{element}</Text>
+            ))
+          }
+        </View>
+    );
+    
+    protein = props.selectedProduct.composition.protein;
+    carbons = props.selectedProduct.composition.carbohydrates;
+    fats = props.selectedProduct.composition.fats;
+  };
   return (
     <Modal onRequestClose={props.onModalClosed} visible={props.selectedProduct !== null} animationType="slide">
       <View style={styles.modalContainer}>
         {modalContent}
+        {ingredients}
+        <PieChart
+          style= {styles.chart}
+          data={
+            [
+              {
+                value: protein,
+                svg: {
+                  fill: '#4CAF50',
+                },
+                key: 'proteins'
+              },
+              {
+                value: carbons,
+                svg: {
+                  fill: '#03A9F4',
+                },
+                key: 'carbons'
+              },
+              {
+                value: fats,
+                svg: {
+                  fill: '#F44336',
+                },
+                key: 'fats'
+              },
+            ]
+          }
+        />
+        <View style={styles.compositionStyle}>
+          <Text style={[styles.compositionTextContainer, {backgroundColor: '#4CAF50'}]}>Białko</Text>
+          <Text style={[styles.compositionTextContainer, {backgroundColor: '#03A9F4'}]}>Węglowodany</Text>
+          <Text style={[styles.compositionTextContainer, {backgroundColor: '#F44336'}]}>Tłuszcze</Text>
+        </View>
         <View>
-          <Button title="Close" onPress={props.onModalClosed} />
+          <Button title="Zamknij" onPress={props.onModalClosed} />
         </View>
       </View>
     </Modal>
@@ -27,7 +79,8 @@ const productDetail = props => {
 
 const styles = StyleSheet.create({
   modalContainer: {
-    margin: 22
+    margin: 22,
+    flex:1,
   },
   productImage: {
     width: '100%',
@@ -36,7 +89,47 @@ const styles = StyleSheet.create({
   productName: {
     fontWeight: 'bold',
     textAlign: 'center',
-    fontSize: 28
+    fontSize: 28,
+    marginBottom: 10,
+  },
+  chart: {
+    height: 150,
+    marginBottom: 10,
+  },
+  compositionStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '100%',
+  },
+  compositionTextContainer: {
+    padding: 5,
+    borderRadius: 5,
+    margin: 5,
+    height: 30,
+    overflow: 'hidden',
+    color: '#FFF',
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  ingredientsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '100%',
+    flexWrap: 'wrap',
+    marginBottom: 10,
+    minHeight: 100,
+  },
+  ingredient: {
+    padding: 5,
+    borderRadius: 5,
+    margin: 5,
+    height: 30,
+    textAlignVertical: 'center',
+    overflow: 'hidden',
+    backgroundColor: '#E0E0E0',
+    fontWeight: '400',
   }
 });
 
--- store/reducers/products.js
+++ store/reducers/products.js
@@ -8,7 +8,21 @@ const initialState = {
         price: '4.00',
         image: {
           uri: 'https://mcdonalds.pl/images/products-og/cheeseburger.png'
-        }
+        },
+        composition: {
+          protein: 10,
+          carbohydrates: 30,
+          fats: 20,
+        },
+        ingredients: [
+          'wołownia',
+          'pikle',
+          'ser cheddar',
+          'cebula szuszona',
+          'mąka',
+          'keczup',
+          'musztarda'
+        ]
       },
       {
         key: `${Math.random()}`,
@@ -16,141 +30,478 @@ const initialState = {
         price: '3.50',
         image: {
           uri: 'https://mcdonalds.pl/images/products-og/hamburger.png'
-        }
+        },
+        composition: {
+          protein: 10,
+          carbohydrates: 30,
+          fats: 10,
+        },
+        ingredients: [
+          'wołownia',
+          'pikle',
+          'cebula szuszona',
+          'mąka',
+          'keczup',
+          'musztarda'
+        ]
       },
       {
+        key: `${Math.random()}`,
+        name: 'Fresh Tasty',
+        price: '4.00',
+        image: {
+          uri: 'https://dsmaipc1fg2ox.cloudfront.net/uploads/20181009104542/icon-salatka-fresh-tasty.png?402'
+        },
+        composition: {
+          protein: 10,
+          carbohydrates: 26,
+          fats: 10,
+        },
+        ingredients: [
+          'wołownia',
+          'mąka',
+          'sałata krojona',
+          'pomidor',
+          'sos Big Tasty'
+        ]
+      },
+      {
+        key: `${Math.random()}`,
+        name: 'Kurczakburger',
+        price: '4.00',
+        image: {
+          uri: 'https://dsmaipc1fg2ox.cloudfront.net/uploads/20160210090950/kurczakburger.png?402'
+        },
+        composition: {
+          protein: 15,
+          carbohydrates: 26,
+          fats: 10,
+        },
+        ingredients: [
+          'kurczak',
+          'mąka',
+          'sałata krojona',
+          'sos paprykowo-pomidorowy',
+        ]
+      },
+      {
+        key: `${Math.random()}`,
+        name: 'McDouble',
+        price: '6.00',
+        image: {
+          uri: 'https://dsmaipc1fg2ox.cloudfront.net/uploads/20160210091317/mcdouble.png?402'
+        },
+        composition: {
+          protein: 30,
+          carbohydrates: 30,
+          fats: 25,
+        },
+        ingredients: [
+          'wołownia',
+          'pikle',
+          'mąka',
+          'ser cheddar',
+          'cebula suszona',
+          'keczup',
+          'musztarda'
+        ]
+      },
+      {
+        key: `${Math.random()}`,
+        name: 'WieśMac',
+        price: '10.00',
+        image: {
+          uri: 'https://dsmaipc1fg2ox.cloudfront.net/uploads/20160210091618/wiesmac.png?402'
+        },
+        composition: {
+          protein: 35,
+          carbohydrates: 30,
+          fats: 15,
+        },
+        ingredients: [
+          'wołownia',
+          'sałata lodowa krojona',
+          'mąka',
+          'ser cheddar topiony',
+          'cebula świeża',
+          'pomidor',
+          'sos musztardowo-chrzanowy'
+        ]
+      },
+      {
+        key: `${Math.random()}`,
+        name: 'McRoyal',
+        price: '11.00',
+        image: {
+          uri: 'https://dsmaipc1fg2ox.cloudfront.net/uploads/20160210091404/mcroyal.png?402'
+        },
+        composition: {
+          protein: 35,
+          carbohydrates: 30,
+          fats: 15,
+        },
+        ingredients: [
+          'wołownia',
+          'pikle',
+          'sałata lodowa krojona',
+          'mąka',
+          'ser cheddar topiony',
+          'cebula świeża',
+          'pomidor',
+          'keczup',
+          'musztarda',
+        ]
+      },
+      {
+        key: `${Math.random()}`,
+        name: 'Podwójny WieśMac',
+        price: '10.00',
+        image: {
+          uri: 'https://dsmaipc1fg2ox.cloudfront.net/uploads/20160210091714/wiesmac-podwojny.png?402'
+        },
+        composition: {
+          protein: 35,
+          carbohydrates: 30,
+          fats: 15,
+        },
+        ingredients: [
+          'wołownia',
+          'sałata lodowa krojona',
+          'mąka',
+          'ser cheddar topiony',
+          'cebula świeża',
+          'pomidor',
+          'sos musztardowo-chrzanowy'
+        ]
+      },
+      {
+        key: `${Math.random()}`,
+        name: 'Podwójny McRoyal',
+        price: '11.00',
+        image: {
+          uri: 'https://dsmaipc1fg2ox.cloudfront.net/uploads/20160210091542/mcroyal-podwojny.png?402'
+        },
+        composition: {
+          protein: 35,
+          carbohydrates: 30,
+          fats: 15,
+        },
+        ingredients: [
+          'wołownia',
+          'pikle',
+          'sałata lodowa krojona',
+          'mąka',
+          'ser cheddar topiony',
+          'cebula świeża',
+          'pomidor',
+          'keczup',
+          'musztarda',
+        ]
+      },
+    ],
+    kfc: [
+      {
+        key: `${Math.random()}`,
+        name: 'McDouble',
+        price: '6.00',
+        image: {
+          uri: 'https://dsmaipc1fg2ox.cloudfront.net/uploads/20160210091317/mcdouble.png?402'
+        },
+        composition: {
+          protein: 30,
+          carbohydrates: 30,
+          fats: 25,
+        },
+        ingredients: [
+          'wołownia',
+          'pikle',
+          'mąka',
+          'ser cheddar',
+          'cebula',
+          'keczup'
+        ]
+      }, {
+        key: `${Math.random()}`,
+        name: 'Hamburger',
+        price: '3.50',
+        image: {
+          uri: 'https://mcdonalds.pl/images/products-og/hamburger.png'
+        },
+        composition: {
+          protein: 100,
+          carbohydrates: 20,
+          fats: 40,
+        },
+        ingredients: [
+          'wołownia',
+          'pikle',
+          'ser cheddar',
+          'cebula',
+          'bułka'
+        ]
+      }, {
         key: `${Math.random()}`,
         name: 'Chips',
         price: '5.00',
         image: {
           uri: 'https://mcdonalds.pl/images/products-og/frytki.png'
-        }
+        },
+        composition: {
+          protein: 190,
+          carbohydrates: 26,
+          fats: 50,
+        },
+        ingredients: [
+          'wołownia',
+          'pikle',
+          'ser cheddar',
+          'cebula',
+          'bułka'
+        ]
       },
     ],
-    kfc: [{
+    burgerKing: [
+      {
         key: `${Math.random()}`,
-        name: 'Strips',
+        name: 'Cheesburger',
         price: '4.00',
         image: {
-          uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/220px-McDonald%27s_Golden_Arches.svg.png'
-        }
-      },
-      {
+          uri: 'https://mcdonalds.pl/images/products-og/cheeseburger.png'
+        },
+        composition: {
+          protein: 100,
+          carbohydrates: 200,
+          fats: 80,
+        },
+        ingredients: [
+          'wołownia',
+          'pikle',
+          'ser cheddar',
+          'cebula',
+          'bułka'
+        ]
+      }, {
         key: `${Math.random()}`,
-        name: 'Wings',
+        name: 'Hamburger',
         price: '3.50',
         image: {
-          uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/220px-McDonald%27s_Golden_Arches.svg.png'
-        }
-      },
-      {
+          uri: 'https://mcdonalds.pl/images/products-og/hamburger.png'
+        },
+        composition: {
+          protein: 100,
+          carbohydrates: 20,
+          fats: 40,
+        },
+        ingredients: [
+          'wołownia',
+          'pikle',
+          'ser cheddar',
+          'cebula',
+          'bułka'
+        ]
+      }, {
         key: `${Math.random()}`,
         name: 'Chips',
-        price: '5.50',
+        price: '5.00',
         image: {
-          uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/220px-McDonald%27s_Golden_Arches.svg.png'
-        }
-      },
-    ],
-    burgerKing: [{
-        key: `${Math.random()}`,
-        name: 'Whooper',
-        price: '4.00',
-        image: {
-          uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/220px-McDonald%27s_Golden_Arches.svg.png'
-        }
-      },
-      {
-        key: `${Math.random()}`,
-        name: 'Whooper Junior',
-        price: '3.50',
-        image: {
-          uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/220px-McDonald%27s_Golden_Arches.svg.png'
-        }
-      },
-      {
-        key: `${Math.random()}`,
-        name: 'Chips',
-        price: '6.00',
-        image: {
-          uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/220px-McDonald%27s_Golden_Arches.svg.png'
-        }
+          uri: 'https://mcdonalds.pl/images/products-og/frytki.png'
+        },
+        composition: {
+          protein: 190,
+          carbohydrates: 26,
+          fats: 50,
+        },
+        ingredients: [
+          'wołownia',
+          'pikle',
+          'ser cheddar',
+          'cebula',
+          'bułka'
+        ]
       },
     ],
     others: {
       'Pasibus': [{
           key: `${Math.random()}`,
-          name: 'Klasyk',
+          name: 'Cheesburger',
           price: '4.00',
           image: {
-            uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/220px-McDonald%27s_Golden_Arches.svg.png'
-          }
+            uri: 'https://mcdonalds.pl/images/products-og/cheeseburger.png'
+          },
+          composition: {
+            protein: 100,
+            carbohydrates: 200,
+            fats: 80,
+          },
+          ingredients: [
+            'wołownia',
+            'pikle',
+            'ser cheddar',
+            'cebula',
+            'bułka'
+          ]
         },
         {
           key: `${Math.random()}`,
-          name: 'Whooper Junior',
+          name: 'Hamburger',
           price: '3.50',
           image: {
-            uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/220px-McDonald%27s_Golden_Arches.svg.png'
-          }
+            uri: 'https://mcdonalds.pl/images/products-og/hamburger.png'
+          },
+          composition: {
+            protein: 100,
+            carbohydrates: 20,
+            fats: 40,
+          },
+          ingredients: [
+            'wołownia',
+            'pikle',
+            'ser cheddar',
+            'cebula',
+            'bułka'
+          ]
         },
         {
           key: `${Math.random()}`,
           name: 'Chips',
-          price: '6.00',
+          price: '5.00',
           image: {
-            uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/220px-McDonald%27s_Golden_Arches.svg.png'
-          }
+            uri: 'https://mcdonalds.pl/images/products-og/frytki.png'
+          },
+          composition: {
+            protein: 190,
+            carbohydrates: 26,
+            fats: 50,
+          },
+          ingredients: [
+            'wołownia',
+            'pikle',
+            'ser cheddar',
+            'cebula',
+            'bułka'
+          ]
         },
       ],
       'Sevi kebab': [{
           key: `${Math.random()}`,
-          name: 'Doner kebab',
+          name: 'Cheesburger',
           price: '4.00',
           image: {
-            uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/220px-McDonald%27s_Golden_Arches.svg.png'
-          }
+            uri: 'https://mcdonalds.pl/images/products-og/cheeseburger.png'
+          },
+          composition: {
+            protein: 100,
+            carbohydrates: 200,
+            fats: 80,
+          },
+          ingredients: [
+            'wołownia',
+            'pikle',
+            'ser cheddar',
+            'cebula',
+            'bułka'
+          ]
         },
         {
           key: `${Math.random()}`,
-          name: 'Whooper Junior',
+          name: 'Hamburger',
           price: '3.50',
           image: {
-            uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/220px-McDonald%27s_Golden_Arches.svg.png'
-          }
+            uri: 'https://mcdonalds.pl/images/products-og/hamburger.png'
+          },
+          composition: {
+            protein: 100,
+            carbohydrates: 20,
+            fats: 40,
+          },
+          ingredients: [
+            'wołownia',
+            'pikle',
+            'ser cheddar',
+            'cebula',
+            'bułka'
+          ]
         },
         {
           key: `${Math.random()}`,
           name: 'Chips',
-          price: '6.00',
+          price: '5.00',
           image: {
-            uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/220px-McDonald%27s_Golden_Arches.svg.png'
-          }
+            uri: 'https://mcdonalds.pl/images/products-og/frytki.png'
+          },
+          composition: {
+            protein: 190,
+            carbohydrates: 26,
+            fats: 50,
+          },
+          ingredients: [
+            'wołownia',
+            'pikle',
+            'ser cheddar',
+            'cebula',
+            'bułka'
+          ]
         },
       ],
       'Kebab King': [{
           key: `${Math.random()}`,
-          name: 'Falafel',
+          name: 'Cheesburger',
           price: '4.00',
           image: {
-            uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/220px-McDonald%27s_Golden_Arches.svg.png'
-          }
+            uri: 'https://mcdonalds.pl/images/products-og/cheeseburger.png'
+          },
+          composition: {
+            protein: 100,
+            carbohydrates: 200,
+            fats: 80,
+          },
+          ingredients: [
+            'wołownia',
+            'pikle',
+            'ser cheddar',
+            'cebula',
+            'bułka'
+          ]
         },
         {
           key: `${Math.random()}`,
-          name: 'Whooper Junior',
+          name: 'Hamburger',
           price: '3.50',
           image: {
-            uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/220px-McDonald%27s_Golden_Arches.svg.png'
-          }
+            uri: 'https://mcdonalds.pl/images/products-og/hamburger.png'
+          },
+          composition: {
+            protein: 100,
+            carbohydrates: 20,
+            fats: 40,
+          },
+          ingredients: [
+            'wołownia',
+            'pikle',
+            'ser cheddar',
+            'cebula',
+            'bułka'
+          ]
         },
         {
           key: `${Math.random()}`,
           name: 'Chips',
-          price: '6.00',
+          price: '5.00',
           image: {
-            uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/220px-McDonald%27s_Golden_Arches.svg.png'
-          }
+            uri: 'https://mcdonalds.pl/images/products-og/frytki.png'
+          },
+          composition: {
+            protein: 190,
+            carbohydrates: 26,
+            fats: 50,
+          },
+          ingredients: [
+            'wołownia',
+            'pikle',
+            'ser cheddar',
+            'cebula',
+            'bułka'
+          ]
         },
       ],
     }
